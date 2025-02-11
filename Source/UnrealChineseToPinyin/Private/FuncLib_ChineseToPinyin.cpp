891291f59a727baec3a079a21ff8bd63da3f4c98
FHangH <752972182@qq.com>
2025-02-11 16:04:29 +0800

    UnrealChineseToPinyin

---
+++ Source/UnrealChineseToPinyin/Private/FuncLib_ChineseToPinyin.cpp
@@ -0,0 +1,20 @@
+﻿#include "FuncLib_ChineseToPinyin.h"
+#include "Pinyin.h"
+
+void UFuncLib_ChineseToPinyin::ChineseToPinYin(const FString& ChineseStr, FString& PinYinStr)
+{
+	if (ChineseStr.IsEmpty()) PinYinStr = {};
+
+	for (const TCHAR& Char : ChineseStr)
+	{
+		auto HexCode = FString::Printf(TEXT("%X"), static_cast<int32>(Char));
+		if (const auto Result = PinYinMap.Find(HexCode))
+		{
+			PinYinStr += FString::Printf(TEXT("%s "), **Result);
+		}
+		else
+		{
+			PinYinStr += Char;
+		}
+	}
+}
---
+++ Source/UnrealChineseToPinyin/Public/FuncLib_ChineseToPinyin.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FuncLib_ChineseToPinyin.generated.h"
+
+UCLASS()
+class UNREALCHINESETOPINYIN_API UFuncLib_ChineseToPinyin : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="FH|ChineseToPinYin")
+	static void ChineseToPinYin(const FString& ChineseStr, FString& PinYinStr);
+};
